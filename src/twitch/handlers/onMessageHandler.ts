4bfc3b5ddf1285d7669b9e49020a6dcaf4195d27
Gaudiot <victor.gaudiot@gmail.com>
2023-09-23 22:28:13 -0300

    connect with auth token & regex command

--- src/twitch/handlers/onMessageHandler.ts
+++ src/twitch/handlers/onMessageHandler.ts
@@ -9,22 +9,28 @@ const botService = container.resolve<BotService>('BotService');
 
 // Called every time a message comes in
 function onMessageHandler (target: string, context: ChatUserstate, message: string, self: boolean): void {
-    if (self || message.length < 2) return; // Ignore messages from the bot
-    botService.setAttributes({
-      target,
-      context
-    });
-    
-    // Remove whitespace from chat message
-    const commandName = message.trim();
-    if(commandName.charAt(0) != COMMAND_CHAR) return;
-    message = removeCommandChar(message);
-    
-    commandsRouter(message);
+  if (self) return; // Ignore messages from the bot
+  const commandRegex = RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
+  const commandMatch = message.match(commandRegex);
+
+  if(!commandMatch) return;
+  const [, command, args] = commandMatch;
+
+  botService.setAttributes({
+    target,
+    context
+  });
+  
+  // Remove whitespace from chat message
+  const commandName = message.trim();
+  if(commandName.charAt(0) != COMMAND_CHAR) return;
+  message = removeCommandChar(message);
+  
+  commandsRouter(message);
 }
 
 function removeCommandChar(message: string): string{
-    return message.substring(1);
+  return message.substring(1);
 }
 
 export default onMessageHandler;
--- src/twitch/services/connect.ts
+++ src/twitch/services/connect.ts
@@ -4,22 +4,25 @@ import TwitchApi from './twitchApi';
 const connect = async () => {
     const twitchApi = new TwitchApi();
 
-    try{
-        const accessToken = await twitchApi.getAccessToken();
-        process.env.AUTH_TOKEN = `oauth:${accessToken}`
-    }catch(err){
-        console.log("[ERR]: ", err);
-    }
+    // try{
+    //     const accessToken = await twitchApi.getAccessToken();
+    //     process.env.AUTH_TOKEN = `oauth:${accessToken}`
+    // }catch(err){
+    //     console.log("[ERR]: ", err);
+    // }
 
     // Define configuration options
-    const opts = {
+    const opts: tmi.Options = {
         identity: {
             username: process.env.USERNAME,
             password: process.env.AUTH_TOKEN
         },
         channels: [
             'gaudiot'
-        ]
+        ],
+        connection: {
+            reconnect: true
+        }
     };
 
     const client = new tmi.client(opts);
